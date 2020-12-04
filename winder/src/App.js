226f59c4a0641da392f81666213504e5f4912150
Thatz Weerakoon <59426115+thatz98@users.noreply.github.com>
2020-12-04 16:02:49 +0530

    Added swipe buttons

--- winder/src/App.js
+++ winder/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import WinderCards from './WinderCards';
+import SwipeButtons from './SwipeButtons';
+
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
 
       <WinderCards />
       
-      {/* SwipeButtons */}
+      <SwipeButtons />
     </div>
   );
 }
